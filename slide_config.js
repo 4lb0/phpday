c1930122db0c3ec53735efb2821bd22b7ba1772f
Rodrigo Arce <albo@pragmore.com>
2014-06-06 17:05:19 -0300

    Initial import

---
+++ slide_config.js
@@ -0,0 +1,39 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: '',
+    subtitle: '',
+    eventInfo: {
+        title: 'PHP Day',
+        date: '31/05/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/pragmore_favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Rodrigo "Albo" Arce',
+    company: 'CTO  Pragmore',
+    // gplus: 'http://plus.google.com/1234567890',
+    www: 'http://pragmore.com',
+    github: 'http://github.com/4lb0'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
